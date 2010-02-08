960e5153addf40a8bb3e8ef16b0860d3fd989c15
Thomas LEROY <tmsleroy@gmail.com>
2010-02-08 18:15:52 +0100

    0.4.2 german

--- get_mo.sh
+++ get_mo.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 LANG_PO="fr"
 LOC_DIR="share/locale/"$LANG_PO"/LC_MESSAGES"
+mkdir "share/locale/"$LANG_PO
+mkdir $LOC_DIR
 msgfmt "po/"$LANG_PO".po" -o $LOC_DIR"/pytomtom.mo"
 
 LANG_PO="it"
 LOC_DIR="share/locale/"$LANG_PO"/LC_MESSAGES"
+mkdir "share/locale/"$LANG_PO
+mkdir $LOC_DIR
+msgfmt "po/"$LANG_PO".po" -o $LOC_DIR"/pytomtom.mo"
+
+LANG_PO="de"
+LOC_DIR="share/locale/"$LANG_PO"/LC_MESSAGES"
+mkdir "share/locale/"$LANG_PO
+mkdir $LOC_DIR
 msgfmt "po/"$LANG_PO".po" -o $LOC_DIR"/pytomtom.mo"
 
 
--- get_po_and_pot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PY_DIR="share/pytomtom/src/pytomtom.py"
-
-xgettext -k_ -kN_ -o "pytomtom.pot" $PY_DIR
-msginit -i pytomtom.pot -o "pytomtom.po"
-
-
-
-
